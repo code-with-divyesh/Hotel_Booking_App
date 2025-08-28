cff62d0254389024ebb13b0c97d8c3d039cf6460
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-08-28 15:10:30 +0530

    some changes for webhook

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,28 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import connectDB from "./Config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import clerkWebHooks from "./Controller/ClearkWebHooks.js";
+
+connectDB();
 
 const app = express();
 app.use(cors());
 
+// Clerk requires raw body for webhooks verification
+app.post(
+  "/api/clerk",
+  express.raw({ type: "application/json" }), // raw body only for webhook
+  clerkWebHooks
+);
+
+// All other routes can use JSON parsing
+app.use(express.json());
+app.use(clerkMiddleware());
+
 app.get("/", (req, res) => {
-  res.send("Api is Working");
+  res.send("API is Working");
 });
 
 const PORT = process.env.PORT || 3000;
