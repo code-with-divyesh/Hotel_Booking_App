c281587f7d2c18c88ab6db9c11954b69d9ed4dbd
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-08-30 13:11:16 +0530

    change id in room model

--- backend/Models/Room.js
+++ backend/Models/Room.js
@@ -2,16 +2,19 @@ import mongoose from "mongoose";
 
 const roomSchema = new mongoose.Schema(
   {
-    hotel:{type:String,ref:"Hotel",required:true},
-    roomType:{type:String,required:true},
-    PricePerNight:{type:Number,required:true},
-    amenities:{type:Array,required:true},
-    images:[{type:String}],
-    isAvailable:{type:Boolean,default:true},
+    hotel: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Hotel",
+      required: true,
+    },
+    roomType: { type: String, required: true },
+    pricePerNight: { type: Number, required: true },
+    amenities: { type: Array, required: true },
+    images: [{ type: String }],
+    isAvailable: { type: Boolean, default: true },
   },
   { timestamps: true }
 );
 const Room = mongoose.models.Room || mongoose.model("Room", roomSchema);
 
 export default Room;
-
