83a3fd72c551a2da611bd557b699f8d81052d26d
code-with-divyesh <gandhidivyesh2308@hotmail.com>
2025-08-30 13:12:40 +0530

    add endpoints in room router

--- backend/Routes/roomRouter.js
+++ backend/Routes/roomRouter.js
@@ -1,10 +1,18 @@
 import express from "express";
 import upload from "../Middleware/uploadMiddleware.js";
 import { protect } from "../Middleware/authMiddleware.js";
-import { createRoom } from "../Controller/roomController.js";
+import {
+  createRoom,
+  getOwnerRooms,
+  getRooms,
+  toggleRoomAvailablity,
+} from "../Controller/roomController.js";
 
 const roomRouter = express.Router();
 
 roomRouter.post("/", upload.array("images", 4), protect, createRoom);
+roomRouter.get("/", getRooms);
+roomRouter.get("/owner", protect, getOwnerRooms);
+roomRouter.post("/toggle-availability", protect, toggleRoomAvailablity);
 
 export default roomRouter;
